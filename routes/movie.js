dbdb76c325a345a41974d49c3242011b184957b4
robertjohnlucero1234 <robertjohnlucero1234@gmail.com>
2024-07-12 19:00:09 +0800

    removed verify - getAllMovies

--- routes/movie.js
+++ routes/movie.js
@@ -4,7 +4,7 @@ const movieController = require("../controllers/movie");
 const { verify, verifyAdmin } = require("../auth");
 
 router.post("/addMovie", verify, verifyAdmin, movieController.addMovie);
-router.get("/getMovies", verify, movieController.getAllMovies);
+router.get("/getMovies", movieController.getAllMovies);
 router.get("/getMovie/:id", verify, movieController.getMovieById);
 router.patch("/updateMovie/:id", verify, verifyAdmin, movieController.updateMovie);
 router.delete("/deleteMovie/:id", verify, verifyAdmin, movieController.deleteMovie);
