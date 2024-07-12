9a3cf3b5b8fc5a772b428139e4206bda44b8fc1f
robertjohnlucero1234 <robertjohnlucero1234@gmail.com>
2024-07-12 18:02:23 +0800

    add getUserDetails

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/user');
+const { verify, verifyAdmin } = require("../auth");
 
 const router = express.Router();
 
 router.post("/register", userController.registerUser);
 router.post('/login', userController.loginUser);
+router.get('/details', verify, userController.getDetails);
 
 module.exports = router;
